7e90a4fbdaf71e943c305e8310524619fa682dfb
Devaux <devauxpieter@gmail.com>
2025-10-06 22:13:31 +0200

    Added more test
    Added more testable tests and added the files for the abstract method

---
+++ Testable/Testable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TestableTests
+{
+    public class Testable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int AddNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public string ConvertToString<T>(T typeToConvert)
+        {
+            if (typeToConvert == null)
+            {
+                return "";
+            }
+
+            return typeToConvert.ToString();
+        }
+
+
+        public virtual bool IsStringLongEnough(string text)
+        {
+            if (text.Length >= 5)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public abstract List<int> PurgeListOfInts(List<int> listOfInts)
+        {
+            return listOfInts.Clear();
+
+        }
+    }
+}
---
+++ Tests/TestableTests.cs
@@ -0,0 +1,51 @@
+using TestableTests;
+using Xunit;
+
+namespace Tests.TestableTest
+{
+    public class TestableTests
+    {
+        [Fact]
+        public void IsSumAdded_ShouldReturnSumOfNumbers_PUBLIC()
+        {
+            var testableTests = new Testable();
+            int result = testableTests.AddNumbers(11, 27);
+            Assert.Equal(38, result);
+        }
+
+        [Fact]
+        public void IsStringReturned_ShouldReturnStringIfIntIsPassed_GENERIC()
+        {
+            var testableTests = new Testable();
+            string result = testableTests.ConvertToString<int>(1);
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void IsStringReturned_ShouldReturnStringIfStringIsPassed_GENERIC()
+        {
+            var testableTests = new Testable();
+            string result = testableTests.ConvertToString<string>("hello");
+            Assert.Equal("hello", result);
+        }
+
+        [Fact]
+        public void IsStringReturned_ShouldReturnStringIfBooleanIsPassed_GENERIC()
+        {
+            var testableTests = new Testable();
+            string result = testableTests.ConvertToString<bool>(true);
+            Assert.Equal("True", result);
+        }
+
+        [Theory]
+        [InlineData("JennyBartsch", true)]
+        [InlineData("Bob", false)]
+        public void IsBoolReturned_ShouldReturnBoolIfStringIsGiven_VIRTUAL(string text, bool expectedResult)
+        {
+            var testableTests = new Testable();
+            bool result = testableTests.IsStringLongEnough(text);
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
